4461e03a9cebe99190c36e22459a2ae306a0a6f3
Raiannbr <67348873+Raiannbr@users.noreply.github.com>
2021-07-29 23:44:59 -0300

    Update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -8,28 +8,32 @@ import React from 'react'
  import {useState, useEffect} from 'react'
  import { Link } from 'react-router-dom';
 
- export default function Categories(props){
+ export default function Categories(){
 
     
      const [productsCategory, setProductsCategory] = useState([]);
 
-    const {categoryByID} = useParams();
-    //console.log(useParams())
+    const {categoriaByID} = useParams();
 
     const getCategory = async () => {
-        let llamada = fetch(`JSON/DataList.json/category/${categoryByID}`);
+        let llamada = fetch("/JSON/DataList.json")
+        // let llamada = fetch(`../JSON/DataList.json`);
         llamada = await llamada;
         llamada = await llamada.text();
-        setProductsCategory(JSON.parse(llamada));
+        const arrayCat = JSON.parse(llamada)
+        // console.log(arrayCat)
+        const catFilter = arrayCat.filter(productos => productos.categoria === categoriaByID)
+
+        setProductsCategory(catFilter);
     }
 
     useEffect(() =>{
         getCategory();
-    },[])
+    },[categoriaByID])
 
     return (
         <div className='containerCategory'>
-            <h1>CATEGORIA {categoryByID}</h1>
+            <h1>CATEGORIA {categoriaByID}</h1>
             {productsCategory.map(element =>{
                 
                 return (
